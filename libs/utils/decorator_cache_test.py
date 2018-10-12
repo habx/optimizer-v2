5950dcbd2f2999777bdf70e026f022e075c6d678
Florent Clairambault <florent@habx.fr>
2018-10-12 02:56:31 +0200

    First draft of grid test

---
+++ libs/utils/decorator_cache_test.py
@@ -0,0 +1,21 @@
+"""Test of DecoratorCache"""
+
+from libs.utils.decorator_cache import DecoratorCache
+
+nb_calls = 0
+
+@DecoratorCache([__file__], 'results/cache')
+def _decorated_function(x):
+    global nb_calls
+    print('computing')
+    nb_calls += 1
+    return x*x
+
+
+def test_decorated_function():
+    # Same behavior when used directly and cached
+    assert _decorated_function(1000) == _decorated_function(1000)
+
+    nb_calls = 0
+    _decorated_function(1000)
+    assert nb_calls == 0
