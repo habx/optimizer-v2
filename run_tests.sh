fffac680dd16b6f05cba056a052554addd1c0f45
Florent Clairambault <florent@habx.fr>
2019-03-18 19:41:09 +0100

    test

--- run_tests.sh
+++ run_tests.sh
@@ -3,11 +3,12 @@
 # This file can be executed on drone or locally by using:
 # $ docker build . -t test && docker run test
 
+# Cutting tests
+tests=$(circleci tests glob "**/*_test.py") ||:
+
 pip install -q --upgrade pip
 pip install -q -r requirements.txt
 
-# Cutting tests
-tests=$(circleci tests glob "**/*_test.py") ||:
 
 # Verbose view
 pytest -v $tests
