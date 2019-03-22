04d60f6226c0a3c8546f1241f00811b4bd2cbc92
XGeffrier <xavier.geffrier@gmail.com>
2019-03-22 20:00:17 +0100

    added output

--- tools/cli.py
+++ tools/cli.py
@@ -12,10 +12,12 @@ def exists_file(parser, arg):
     else:
         return arg
 
+
 if __name__ == '__main__':
     import argparse
     import logging
     from libs.utils.executor import Executor
+    import json
 
     # arg parser
     parser = argparse.ArgumentParser(description="Optimizer V2")
@@ -25,6 +27,8 @@ if __name__ == '__main__':
     parser.add_argument("-s", dest="setup", required=True, metavar="FILE",
                         type=lambda x: exists_file(parser, x),
                         help="the input setup file path")
+    parser.add_argument("-o", dest="output", required=True,
+                        help="the input solution dir")
     parser.add_argument("-g", dest="grid", required=False,
                         help="grid type", default="optimal_grid")
     parser.add_argument("-u", dest="shuffle", required=False,
@@ -32,6 +36,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     lot_path = args.lot
     setup_path = args.setup
+    output_dir = args.output
     grid_type = args.grid
     shuffle_type = args.shuffle
 
@@ -40,5 +45,6 @@ if __name__ == '__main__':
     executor = Executor()
     executor.set_execution_parameters(grid_type=grid_type, shuffle_type=shuffle_type)
     response = executor.run_from_file_paths(lot_path, setup_path)
-    logging.info("Time: %i", int(response.elapsed_time))
-    logging.info("Nb solutions: %i", len(response.solutions))
+    for i, solution in enumerate(response.solutions):
+        solution_path = os.path.join(output_dir, "output{}.json".format(i))
+        json.dump(solution, solution_path, indent=2, sort_keys=True)
