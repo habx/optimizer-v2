d17bb8c18bbbd30c90a1923b55ccbc5bc1d5e122
Florent Clairambault <florent@habx.fr>
2019-03-28 12:23:19 +0100

    Tiny fix

--- bin/worker.py
+++ bin/worker.py
@@ -301,7 +301,9 @@ def _local_dev_hack():
     if not os.path.exists(os.path.join(os.getenv('HOME'), '.aws/credentials'))\
             and not os.getenv('AWS_ACCESS_KEY_ID') \
             and not os.getenv('HABX_ENV'):
-        logging.warning("[LOCAL DEV ONLY] Injecting some AWS credentials [LOCAL DEV ONLY]")
+        logging.warning(
+            "[LOCAL DEV ONLY] Injecting some AWS credentials in env vars [/LOCAL DEV ONLY]"
+        )
         os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAJLYSRVQ2B5NDHC3A'
         os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
         os.environ['AWS_DEFAULT_REGION'] = 'eu-west-1'
