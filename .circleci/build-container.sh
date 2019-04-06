3e24c0ddb2f241a94e341c916abf78c3cc6df032
Florent Clairambault <florent@habx.fr>
2019-04-06 23:32:05 +0200

    OPT-47: Test

--- .circleci/build-container.sh
+++ .circleci/build-container.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-cd /home/circleci
 export BUILD_REF=$(if [ -z "$CIRCLE_TAG" ]; then echo ${CIRCLE_BRANCH/\//-}-${CIRCLE_SHA1:0:7}; else echo $CIRCLE_TAG; fi)
 export PLUGIN_TAGS=${BUILD_REF}
 export PLUGIN_REPO=724009402066.dkr.ecr.eu-west-1.amazonaws.com/worker-optimizer-v2
