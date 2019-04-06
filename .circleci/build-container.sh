117a4859e597c49f504f9f7674184e338ee874f4
Florent Clairambault <florent@habx.fr>
2019-04-06 09:11:28 +0200

    OPT-47: Test

---
+++ .circleci/build-container.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /home/circleci
+export BUILD_REF=$(if [ -z "$CIRCLE_TAG" ]; then echo ${CIRCLE_BRANCH/\//-}-${CIRCLE_SHA1:0:7}; else echo $CIRCLE_TAG; fi)
+export PLUGIN_TAGS=${BUILD_REF}
+export PLUGIN_REPO=724009402066.dkr.ecr.eu-west-1.amazonaws.com/worker-optimizer-v2
+/usr/local/bin/dockerd-entrypoint.sh /bin/drone-docker-ecr
