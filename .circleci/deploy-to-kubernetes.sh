66d7d6fb454981fc82295adeadd267e5f4a55451
Florent Clairambault <florent@habx.fr>
2019-04-08 12:36:20 +0200

    OPT-47: test

--- .circleci/deploy-to-kubernetes.sh
+++ .circleci/deploy-to-kubernetes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 
-export PLUGIN_REPO=724009402066.dkr.ecr.eu-west-1.amazonaws.com/${CIRCLE_PROJECT_REPONAME}
 export CIRCLE_PROJECT_REPONAME=worker-optimizer-v2
+export PLUGIN_REPO=724009402066.dkr.ecr.eu-west-1.amazonaws.com/${CIRCLE_PROJECT_REPONAME}
 export PLUGIN_CONTAINER=${CIRCLE_PROJECT_REPONAME}
 export PLUGIN_DEPLOYMENT=${CIRCLE_PROJECT_REPONAME}
 
