e1064af56bc98dcd0559beeff970243847d77607
Florent Clairambault <florent@habx.fr>
2019-04-11 02:05:44 +0200

    OPT-85: Executor refactoring

---
+++ libs/optimizer.py
@@ -0,0 +1,191 @@
+# coding=utf-8
+"""
+module used to run optimizer
+"""
+
+import logging
+from typing import List, Dict
+import time
+import json
+
+from libs.io import reader
+from libs.io.writer import generate_output_dict
+from libs.modelers.grid import GRIDS
+from libs.modelers.seed import SEEDERS
+from libs.modelers.shuffle import SHUFFLES
+from libs.space_planner.space_planner import SpacePlanner
+from libs.version import VERSION as OPTIMIZER_VERSION
+
+
+class Response:
+    """
+    Response of an optimizer run. Contains solutions and all run data.
+    """
+
+    def __init__(self,
+                 solutions: List[dict],
+                 elapsed_times: Dict[str, float]):
+        self.solutions = solutions
+        self.elapsed_times = elapsed_times
+
+    @property
+    def as_dict(self) -> dict:
+        """
+        return all response data in a dict
+        """
+        return self.__dict__
+
+    def to_json_file(self, filepath) -> None:
+        """
+        save all response data in a json file
+        """
+        with open(filepath, "w") as output_file:
+            json.dump(self.as_dict, output_file, sort_keys=True, indent=2)
+
+
+class ExecParams:
+    """Dict wrapper, mostly useful for auto-completion"""
+    def __init__(self, params):
+        if not params:
+            params = {}
+
+        self.grid_type = params.get('grid_type', 'optimal_grid')
+        self.seeder_type = params.get('seeder_type', "simple_seeder")
+        self.do_plot = params.get('do_plot', False)
+        self.shuffle_type = params.get('shuffle_type', 'bedrooms_corner')
+        self.do_shuffle = params.get('do_shuffle', False)
+
+
+class Optimizer:
+    """
+    Class used to run Optimizer with defined parameters.
+    """
+
+    VERSION = OPTIMIZER_VERSION
+    """Current version"""
+
+    def run_from_file_names(self,
+                            lot_file_name: str = "011.json",
+                            setup_file_name: str = "011_setup0.json",
+                            params: dict = None) -> Response:
+        """
+        Run Optimizer from file names.
+        :param lot_file_name: name of lot file, file has to be in resources/blueprints
+        :param setup_file_name: name of setup file, file has to be in resources/specifications
+        :param params: Execution parameters
+        :return: optimizer response
+        """
+        lot = reader.get_json_from_file(lot_file_name)
+        setup = reader.get_json_from_file(setup_file_name,
+                                          reader.DEFAULT_SPECIFICATION_INPUT_FOLDER)
+
+        return self.run(lot, setup, params)
+
+    def run(self, lot: dict, setup: dict, params_dict: dict = None) -> Response:
+        """
+        Run Optimizer
+        :param lot: lot data
+        :param setup: setup data
+        :param params_dict: execution parameters
+        :return: optimizer response
+        """
+        assert "v2" in lot.keys(), "lot must contain v2 data"
+
+        params = ExecParams(params_dict)
+
+        # times
+        elapsed_times = {}
+        t0_total = time.process_time()
+        t0_total_real = time.time()
+
+        # reading lot
+        logging.info("Read lot")
+        t0_reader = time.process_time()
+        plan = reader.create_plan_from_data(lot)
+        elapsed_times["reader"] = time.process_time() - t0_reader
+        logging.info("Lot read in %f", elapsed_times["reader"])
+
+        # grid
+        logging.info("Grid")
+        t0_grid = time.process_time()
+        GRIDS[params.grid_type].apply_to(plan)
+        elapsed_times["grid"] = time.process_time() - t0_grid
+        logging.info("Grid achieved in %f", elapsed_times["grid"])
+
+        # seeder
+        logging.info("Seeder")
+        t0_seeder = time.process_time()
+        SEEDERS[params.seeder_type].apply_to(plan)
+
+        if params.do_plot:
+            plan.plot()
+        elapsed_times["seeder"] = time.process_time() - t0_seeder
+        logging.info("Seeder achieved in %f", elapsed_times["seeder"])
+
+        # reading setup
+        logging.info("Read setup")
+        t0_setup = time.process_time()
+        spec = reader.create_specification_from_data(setup)
+        logging.debug(spec)
+        spec.plan = plan
+        spec.plan.remove_null_spaces()
+        elapsed_times["setup"] = time.process_time() - t0_setup
+        logging.info("Setup read in %f", elapsed_times["setup"])
+
+        # space planner
+        t0_space_planner = time.process_time()
+        logging.info("Space planner")
+        space_planner = SpacePlanner("test", spec)
+        best_solutions = space_planner.solution_research()
+        logging.debug(best_solutions)
+        elapsed_times["space planner"] = time.process_time() - t0_space_planner
+        logging.info("Space planner achieved in %f", elapsed_times["space planner"])
+
+        # shuffle
+        t0_shuffle = time.process_time()
+        if params.do_shuffle:
+            logging.info("Shuffle")
+            if best_solutions:
+                for sol in best_solutions:
+                    SHUFFLES[params.shuffle_type].apply_to(sol.plan)
+                    if params.do_plot:
+                        sol.plan.plot()
+        elapsed_times["shuffle"] = time.process_time() - t0_shuffle
+        logging.info("Shuffle achieved in %f", elapsed_times["shuffle"])
+
+        # output
+        t0_output = time.process_time()
+        logging.info("Output")
+        solutions = [generate_output_dict(lot, sol) for sol in best_solutions]
+        elapsed_times["output"] = time.process_time() - t0_output
+        logging.info("Output written in %f", elapsed_times["output"])
+
+        elapsed_times["total"] = time.process_time() - t0_total
+        elapsed_times["totalReal"] = time.time() - t0_total_real
+        logging.info("Run complete in %f (process time), %f (real time)",
+                     elapsed_times["total"],
+                     elapsed_times["totalReal"])
+
+        return Response(solutions, elapsed_times)
+
+
+if __name__ == '__main__':
+    def main():
+        """
+        Useful simple main
+        """
+        logging.getLogger().setLevel(logging.INFO)
+        executor = Optimizer()
+        response = executor.run_from_file_names(
+            "012.json",
+            "012_setup0.json",
+            {
+                "grid_type": "optimal_grid",
+                "do_plot": False,
+            }
+        )
+        logging.info("Time: %i", int(response.elapsed_times["total"]))
+        logging.info("Nb solutions: %i", len(response.solutions))
+
+
+    main()
