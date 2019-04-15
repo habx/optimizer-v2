b0477c03212fa0fc533b9527c9c51cab8334893d
GraziellaL <graziella@habx.fr>
2019-04-15 11:51:56 +0200

    Merge branch 'dev' of github.com:habx/ds-tool-optimizer-exp into feature/circulation-spaces
    # Conflicts:
    #	libs/space_planner/constraints_manager.py

--- libs/space_planner/space_planner.py
+++ libs/space_planner/space_planner.py
@@ -215,8 +215,8 @@ if __name__ == '__main__':
         Test
         :return:
         """
-        input_file = reader.get_list_from_folder(DEFAULT_BLUEPRINT_INPUT_FOLDER)[plan_index]
-        #input_file = "059.json"
+        #input_file = reader.get_list_from_folder(DEFAULT_BLUEPRINT_INPUT_FOLDER)[plan_index]
+        input_file = "059.json"
         t00 = time.process_time()
         plan = reader.create_plan_from_file(input_file)
         # logging.info("input_file %s", input_file)
