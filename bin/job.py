a7bced50fd643c0072b289f355301ec7b2efdaec
Fabien CÔTÉ <fabien.cote@me.com>
2019-04-26 14:02:01 +0200

    Fix (Job.py): use HABX_ENV var

--- bin/job.py
+++ bin/job.py
@@ -34,7 +34,7 @@ def fetch_task_definition(context: dict) -> TaskDefinition:
     endpoint = endpoints.get(os.getenv('HABX_ENV', 'local'))
 
     response = requests.get(endpoint, params=context, headers={
-        'x-habx-token': 'ymSC4QkHwxEnAeyBu9UqWzbs'
+        'x-habx-token': os.getenv('HABX_TOKEN', 'ymSC4QkHwxEnAeyBu9UqWzbs')
     })
 
     job_input = response.json().get('job')
