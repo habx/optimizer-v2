02c85f2e26cb4742ba43b8c7c5486d1d6940d2f7
Florent Clairambault <florent@habx.fr>
2019-05-13 15:36:43 +0200

    OPT-119: Fixes
    Previous issues were caused by OPT-116

--- libs/optimizer_test.py
+++ libs/optimizer_test.py
@@ -1,7 +1,6 @@
 import os
 
-# OPT-119: Making sure the optimizer.py file still works
-
+# OPT-119: Making sure the optimizer.py still works
 rc = os.system('PYTHONPATH=$(pwd) libs/optimizer.py')
 
 assert rc == 0
