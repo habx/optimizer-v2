93d6e3192d498266ef2e3545e15d824b2715e00e
GraziellaL <graziella@habx.fr>
2019-05-14 09:58:24 +0200

    bug

--- libs/space_planner/circulation_test.py
+++ libs/space_planner/circulation_test.py
@@ -10,7 +10,7 @@ from libs.modelers.seed import SEEDERS
 from libs.modelers.grid import GRIDS
 from libs.space_planner.circulation import Circulator, COST_RULES
 
-from libs.space_planner.space_planner import SpacePlanner
+from libs.space_planner.space_planner import SPACE_PLANNERS
 
 test_files = [("011.json", "011_setup0.json")]
 
@@ -30,8 +30,8 @@ def test_circulation(input_file, input_setup):
     spec = reader.create_specification_from_file(input_setup)
     spec.plan = plan
 
-    space_planner = SpacePlanner("test", spec)
-    best_solutions = space_planner.solution_research()
+    space_planner = SPACE_PLANNERS["standard_space_planner"]
+    best_solutions = space_planner.apply_to(spec)
 
     if space_planner.solutions_collector.solutions:
         for solution in best_solutions:
