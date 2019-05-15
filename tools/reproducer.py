16a036f0907be91d3dcd545209ded7b0c773c7e2
Florent Clairambault <florent@habx.fr>
2019-05-15 20:08:23 +0200

    OPT-123: Adding a simple reproducer

---
+++ tools/reproducer.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import logging
+import os
+
+from libs.executor.defs import TaskDefinition
+from libs.executor.executor import Executor
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)-15s | %(lineno)-5d | %(levelname).4s | %(message)s",
+)
+
+parser = argparse.ArgumentParser(description='Fetch task and run it locally')
+
+parser.add_argument('task_id',
+                    help='Task identifier, mandatory.')
+parser.add_argument('-e',
+                    '--env',
+                    dest='env',
+                    help='Target environment.',
+                    choices=['dev', 'staging', 'prod'],
+                    default='dev')
+parser.add_argument('-t',
+                    '--token',
+                    dest='habx_token',
+                    help='habx token to use',
+                    default=os.environ.get('HABX_TOKEN', 'ZBnV3nXJhdqehDmtWT'),
+                    required=False)
+
+args = parser.parse_args()
+
+task_id: str = args.task_id
+env: str = args.env
+habx_token: str = args.habx_token
+
+
+task_dir = 'tasks/%s' % task_id
+
+if not os.path.exists(task_dir):
+    os.makedirs(task_dir)
+    cmd = "opt-replay {task_id} --env {env} --token {token} -o {task_dir}".format(
+            task_id=task_id,
+            env=env,
+            token=habx_token,
+            task_dir=task_dir,
+        )
+    logging.info("Executing %s", cmd)
+    assert os.system(cmd) == 0
+
+
+executor = Executor()
+td = TaskDefinition()
+td.local_context.output_dir = task_dir
+
+with open('%s/blueprint.json' % task_dir, 'r') as blueprint_fp:
+    td.blueprint = json.load(blueprint_fp)
+
+with open('%s/setup.json' % task_dir, 'r') as setup_fp:
+    td.setup = json.load(setup_fp)
+
+with open('%s/params.json' % task_dir, 'r') as params_fp:
+    td.params = json.load(params_fp)
+
+executor.run(td)
