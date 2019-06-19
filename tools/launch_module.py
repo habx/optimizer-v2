7fc0de1926bc4b8fd5a31976354b250721347954
Nicolas POZIN <nicolas@habx.fr>
2019-06-19 10:52:59 +0200

    door test

--- tools/launch_module.py
+++ tools/launch_module.py
@@ -8,12 +8,14 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--module", help="choose launched module",
-                        default="modelers/seed")
+                        default="equipments/doors")
     args = parser.parse_args()
     module = args.module
     files = reader.get_list_from_folder("../resources/blueprints")
     files = [x for x in files if x.endswith('.json')]
     num_files = len(files)
     for index_plan in range(num_files):
+        #if index_plan < 7:
+        #    continue
         command_lauch_grid = "python3 ../libs/" + module + ".py -p " + str(index_plan)
         os.system(command_lauch_grid)
