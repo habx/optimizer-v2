732295772ea734aef97a767e69df8a8d28eb0321
Nicolas POZIN <nicolas@habx.fr>
2019-07-01 11:11:12 +0200

    adapt test

--- libs/space_planner/space_planner_test.py
+++ libs/space_planner/space_planner_test.py
@@ -38,7 +38,7 @@ def test_space_planner(input_file, input_setup):
         assert len(space_planner.solutions_collector.solutions) == 5
         assert len(best_solutions) == 2
     elif input_file == "012.json":
-        assert len(space_planner.solutions_collector.solutions) == 153
+        assert len(space_planner.solutions_collector.solutions) == 209
         assert len(best_solutions) == 2
 
 
