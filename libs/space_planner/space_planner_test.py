a1fd4877a2a4c3ddaf1abda1434763f28e4c3f4e
Bastien Dolla <bastien@habx.fr>
2019-07-09 18:31:59 +0200

    Fixed tests

--- libs/space_planner/space_planner_test.py
+++ libs/space_planner/space_planner_test.py
@@ -35,11 +35,11 @@ def test_space_planner(input_file, input_setup):
     best_solutions = space_planner.apply_to(spec, 3)
 
     if input_file == "009.json":
-        assert len(space_planner.solutions_collector.solutions) == 55
+        assert len(space_planner.solutions_collector.solutions) == 63
         assert len(best_solutions) == 3
     elif input_file == "012.json":
-        assert len(space_planner.solutions_collector.solutions) == 597
-        assert len(best_solutions) == 3
+        assert len(space_planner.solutions_collector.solutions) == 122
+        assert len(best_solutions) == 2
 
 
 def test_duplex():
