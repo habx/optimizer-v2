1f051225fe7b89adf4a77d5067dcfada9e505a68
Nicolas POZIN <nicolas@habx.fr>
2019-07-10 16:31:52 +0200

    adapt test

--- libs/space_planner/space_planner_test.py
+++ libs/space_planner/space_planner_test.py
@@ -39,7 +39,7 @@ def test_space_planner(input_file, input_setup):
         assert len(best_solutions) == 3
     elif input_file == "012.json":
         assert len(space_planner.solutions_collector.solutions) == 130
-        assert len(best_solutions) == 3
+        assert len(best_solutions) == 2
 
 
 def test_duplex():
